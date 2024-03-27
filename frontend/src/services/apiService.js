2fa4de6b93496937390c3a291d76a88c02719beb
Hyunmyung Park <hmp0504@gmail.com>
2024-03-27 13:44:50 -0600

    changed filed directory

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 // src/services/apiService.js
 import axios from 'axios';
 
-const BASE_URL = 'https://dummyjson.com/products';
+const BASE_URL = 'http://localhost:5000/products';
 
 // Fetches details for a single product by its ID
 export const getProductDetails = async (id) => {
